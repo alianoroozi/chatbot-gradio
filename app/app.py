b0b2c6eddd9fe718c27773c8c889245f10ed11fe
Ali Noroozi <aliasghar.noroozi@gmail.com>
2024-04-15 18:18:46 -0400

    Add anthropic model

--- app/app.py
+++ app/app.py
@@ -1,30 +1,50 @@
 from openai import OpenAI
+import anthropic
 import gradio as gr
 from dotenv import load_dotenv
 
 
 load_dotenv()
-client = OpenAI()
-
 OPENAI_MODEL_NAME = "gpt-4-turbo-preview"
-def predict(message, history):
-    history_openai_format = []
-    for human, assistant in history:
-        history_openai_format.append({"role": "user", "content": human })
-        history_openai_format.append({"role": "assistant", "content":assistant})
-    history_openai_format.append({"role": "user", "content": message})
-  
-    response = client.chat.completions.create(
-         model=OPENAI_MODEL_NAME,
-         messages= history_openai_format,
-         temperature=1.0,
-         stream=True
-    )
+CLAUDE_MODEL_NAME = "claude-3-opus-20240229"
 
-    partial_message = ""
-    for chunk in response:
-        if chunk.choices[0].delta.content is not None:
-              partial_message = partial_message + chunk.choices[0].delta.content
-              yield partial_message
+openai_client = OpenAI()
+anthropic_client = anthropic.Anthropic()
+
+def predict(query, history, model_type='openai'):
+    messages = []
+    for human, assistant in history:
+        messages.append({"role": "user", "content": human })
+        messages.append({"role": "assistant", "content": assistant})
+    messages.append({"role": "user", "content": query})
+
+    if model_type == 'openai':
+        response = openai_client.chat.completions.create(
+            model=OPENAI_MODEL_NAME,
+            messages=messages,
+            temperature=0,
+            stream=True
+        )
+
+        partial_message = ""
+        for chunk in response:
+            if chunk.choices[0].delta.content is not None:
+                partial_message = partial_message + chunk.choices[0].delta.content
+                yield partial_message
+
+    elif model_type == 'claude':
+        response = anthropic_client.messages.stream(
+            model=CLAUDE_MODEL_NAME,
+            max_tokens=1000,
+            temperature=0,
+            messages=[]
+        )
+
+        with response as stream:
+            for text in stream.text_stream:
+                yield text
+    else:
+        raise ValueError("model_type must be either 'openai' or 'claude'")
+
 
 gr.ChatInterface(predict).launch(share=True)
